f075663a749ba2f63a2a083c1a66d566e5f960cf
Nirav Surajlal <44361318+NiravSurajlal@users.noreply.github.com>
2022-05-15 10:25:55 +0200

    Add files via upload

--- pipeline.py
+++ pipeline.py
@@ -9,6 +9,10 @@ class Pipeline:
         self.algorithms = {}
         self.data = {}
         self.errors = {}
+        self.pair_plots = {}
+        self.scatter_plots = {}
+        self.scalers = {}
+        self.predictions = {}
 
     def add_algorithm(self, algorithm, algo_name):
         """ Adds OBJECTS to the pipeline algorithms dict."""
@@ -20,17 +24,15 @@ class Pipeline:
     def add_data(self, name, data):
         """ Takes data in the form of a dataframe and stores it in a dict """
         if name in ['train', 'test', 'validation']:
-            self.data[name] = data
+            self.data[name] = data.copy()
             self.original_data = self.data.copy()
         else:
             print('Name must be: train, test or validation.')
     
-    def view_pairlot(self, data, hue=None):
+    def view_pairlot_of_data(self, dataset_name, hue=None):
         """ Uses data from self.data and returns a pairplot"""
-        fig = plt.figure()
-        sns.set_style("whitegrid");
-        sns.pairplot(data, hue=hue, height=2);
-        return fig
+        fig = sns.pairplot(self.data[dataset_name], hue=hue, height=2)
+        self.pair_plots[dataset_name] = fig.figure
     
     def check_num_uniques(self, dataset_name):
         """ Prints the number of unique values in a dataframe per column """
@@ -40,6 +42,12 @@ class Pipeline:
         """ Prints the number of nans in a dataframe per column """
         print(self.data[dataset_name].isna().sum())
     
+    def assess_data(self, dataset_name):
+        print("\t\t NUM UNIQUES:")
+        self.check_num_uniques(dataset_name)
+        print("\t\t NUM NANs:")
+        self.check_nans(dataset_name)
+
     def adjust_data(self, data, dataset_name) -> None:
         """ Allows a readjustment of the data """
         self.data[dataset_name] = data
@@ -55,46 +63,63 @@ class Pipeline:
                 unique_vals = df[col].unique()
                 reassign_dict = {unique_vals[i]: i for i in range(0, len(unique_vals), 1)}
                 df = df.replace({col: reassign_dict})
-        self.data[col]=df
+        self.data[dataset_name] = df
     
-    def scale_data(self, dataset_name, column_names, scaler):
+    def scale_data(self, dataset_name, column_names, scaler_obj):
         """ Scales the data and overwrites self.data dict. Only works with sklearn
             methods that use .fit() then .transform(). Also stores the used scalers
             in a dict {column name: scaler object}"""
-        self.scalers = {}
         data = self.data[dataset_name].copy()
         for col in column_names:
-            scaler.fit(np.array(data[col]).reshape(-1,1))
-            data[col] = scaler.transform()
-            self.scalers[col] = scaler
+            X = np.array(data[col]).reshape(-1,1)
+            scaler_obj.fit(X)
+            data[col] = scaler_obj.transform(X)
+            self.scalers[col] = scaler_obj
         self.data[dataset_name] = data    
 
     def set_labels(self, dataset_name, label_column):
         """ Returns the features and lable dataframes. Only used during supervised/
             reinforcement learning. """
         data = self.data[dataset_name].copy()
-        labels = data[label_column]
-        features = data.drop(columns=label_column)
-        return features, labels
+        self.labels = data[label_column]
+        self.features = data.drop(columns=label_column)
+        return self.features, self.labels
 
-    def get_errors(self, labels, predictions):
+    def get_errors(self, predictions, estimator_name):
         """ Returns average errors & how the error changes. Only used during supervised/
-            reinforcement learning. """
-        errors = np.array()
+            reinforcement learning. Saves to self.errors per estimator as
+            (error, error_change per prediction)"""
+        labels = self.labels.copy()
+        errors = np.empty(shape=(1,1))
         errors = np.append(errors, np.array(predictions-labels))
         error = np.mean(errors)
-        avg_error_over_time = np.array()
+        avg_error_over_time = np.empty(shape=(1,1))
         for i in range(0, len(errors)):
             total = np.sum(errors[0:i])
-            avg_error_over_time = np.append(total/(i+1))
-        return error, avg_error_over_time
+            avg_error_over_time = np.append(avg_error_over_time, total/(i+1))
+        self.errors[estimator_name] = {'error': errors, 'delta_error': avg_error_over_time}
     
-    def plot_scatter(self, y, x=None):
+    def plot_scatter(self, y, name, x=None):
         """ Returns figure object of the scatter plot. """
         fig = plt.figure()
         if x is None:
             plt.scatter(np.arange(0, len(y)), (y), marker='.')
         else:
             plt.scatter((x), (y), marker='.')
-        return fig
-        
+        self.scatter_plots[name] = fig
+    
+    def remove_columns(self, dataset_name, columns_to_drop):
+        """ Takes list of column names and drops them from the
+            required dataset, overwriting it. """
+        print("Data shape:", np.shape(self.data[dataset_name]))
+        for col in columns_to_drop:
+            self.data[dataset_name].drop(columns=[col], inplace=True)
+        print("Data shape:", np.shape(self.data[dataset_name]))
+
+    def remove_na_rows(self, dataset_name):
+        print("Data shape:", np.shape(self.data[dataset_name]))
+        self.data[dataset_name].dropna(inplace=True)
+        print("Data shape:", np.shape(self.data[dataset_name]))
+
+    def add_predictions(self, name, predictions):
+        self.predictions[name] = predictions
